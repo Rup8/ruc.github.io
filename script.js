e964fb8195a79949c367e81fd9e88c98bc02eab5
Rup8 <60883041+Rup8@users.noreply.github.com>
2024-08-02 13:05:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');   
+
+    }
+});
 // Aquí irá el código JavaScript para el menú y otras interacciones
